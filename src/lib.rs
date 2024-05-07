996c4a28cd6a1f5b55cde6a2e5d91050503b8e27
Your Name <you@example.com>
2024-05-07 13:05:55 +1000

    feature: add password generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-mod opts;
-mod process;
+pub mod opts;
+pub mod process;
 pub use opts::{Opts, SubCommand};
 pub use process::process_csv;
+pub use process::process_genpass;
--- src/opts.rs
+++ src/opts.rs
@@ -11,6 +11,8 @@ pub struct Opts {
 pub enum SubCommand {
     #[command(name = "csv", about = "Show CSV, or covert CSV to other formats")]
     Csv(CsvOpts),
+    #[command(name = "genpass", about = "Generate a ramdom password")]
+    Genpass(GenPassOpts),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -35,6 +37,20 @@ pub struct CsvOpts {
 
     #[arg(long, default_value_t = true)]
     pub header: bool,
+}
+
+#[derive(Debug, Parser)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub number: bool,
+    #[arg(long, default_value_t = true)]
+    pub symbol: bool,
 }
 
 fn verify_input_file(filename: &str) -> Result<String, &'static str> {
